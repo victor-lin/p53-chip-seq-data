f989a822fb385467575ff94edd9d5f31583eb86c
Victor Lin <vlin@ufl.edu>
2016-11-29 13:43:54 -0500

    master_table.py cleanup

--- master_table.py
+++ master_table.py
@@ -1,12 +1,12 @@
 import sys
-import pandas as pd
+from pandas import read_table
 from pandasql import sqldf
 from optparse import OptionParser
 from numpy import log10
 from Bio import SeqIO
 
 
-def anno_category(s):
+def anno_category(detailed_anno):
     """Return annotation category from detailed annotation.
 
     Assumes each detailed annotation has only one category name
@@ -18,19 +18,34 @@ def anno_category(s):
     non_repeats = {'TSS', 'TTS', 'exon', "5' UTR", "3' UTR",
                    'CpG', 'intron', 'Intergenic', 'non-coding'}
     other = {'Other', 'Unknown'}
-    lp_idx = s.find('(')
-    if lp_idx > 0:
-        s = s[:lp_idx]
-    for cat in (repeats | non_repeats | other):
-        if cat in s:
-            return cat
+    left_paren_index = detailed_anno.find('(')
+    if left_paren_index > 0:
+        detailed_anno = detailed_anno[:left_paren_index]
+    for category in (repeats | non_repeats | other):
+        if category in detailed_anno:
+            return category
+
+
+def get_anno_table(anno_file):
+    """Return DataFrame from annotation file.
+
+    Variables
+    ---------
+    key : columns to sort by
+    other_cols : additional columns to include from the annotation file
+    """
+    key = ['Chr', 'Start', 'End']
+    other_cols = ['Detailed Annotation', 'Gene Name']
+    anno = read_table(anno_file)
+    anno = anno[key + other_cols].sort_values(by=key)
+    return anno
 
 
 def generate_master_table(options):
     # get peaks
-    peaks = pd.read_table(options.merged_file,
-                          header=None, names=("chr", "start", "end"))
-    samples = pd.read_table(options.samples_file)
+    peaks = read_table(options.merged_file,
+                       header=None, names=('chr', 'start', 'end'))
+    samples = read_table(options.samples_file)
     samples.rename(columns={col: 'sample_' + col
                             for col in ('chr', 'start', 'end', 'length')},
                    inplace=True)
@@ -41,14 +56,17 @@ def generate_master_table(options):
                                 FROM peaks AS p
                                 JOIN samples AS s
                                      ON (s.sample_start >= p.start
-                                         AND s.sample_end <= p.end)""", locals())
+                                         AND s.sample_end <= p.end)""",
+                           locals())
     grouped = merged_samples.groupby(['chr', 'start', 'end'])
     tbl['Sample Count'] = grouped.count()['Sample_Name'].values
     for sample_name in set(samples.Sample_Name):
         tbl[sample_name] = 0
 
     for table_i, row in tbl.iterrows():
-        sys.stdout.write('\rUpdating columns for row {}'.format(table_i + 1))
+        sys.stdout.write('\rUpdating sample {} columns for '
+                         'row {}'.format(options.sample_col,
+                                         table_i + 1))
         sys.stdout.flush()
         key = tuple(list(row.values)[:3])
         group = grouped.get_group(key)
@@ -59,9 +77,7 @@ def generate_master_table(options):
                 tbl.loc[table_i, s] = group['fold_enrichment'].iloc[i]
     print
 
-    anno = pd.read_table(options.anno_file)
-    anno = anno[['Chr', 'Start', 'End',
-                 'Detailed Annotation', 'Gene Name']].sort_values(by=['Chr', 'Start', 'End'])
+    anno = get_anno_table(options.anno_file)
     tbl['Detailed Annotation'] = anno['Detailed Annotation']
     tbl['Annotation Category'] = tbl['Detailed Annotation'].apply(anno_category)
     tbl['Gene Name'] = anno['Gene Name']
@@ -71,18 +87,19 @@ def generate_master_table(options):
                                    'fold_enrichment': max}).reset_index()
 
     # aggregate matrices by regions
-    mast_out = pd.read_table(options.mast_file)
+    mast_out = read_table(options.mast_file)
     mast_out['matrix_score'] = -log10(mast_out['hit_p.value'])
     merged_mast = sqldf("""SELECT *, mo.ROWID AS matrix_id
                              FROM peaks AS p
                              JOIN mast_out AS mo
                                   ON (mo.hit_start >= p.start
-                                      AND mo.hit_end <= p.end)""", locals())
+                                      AND mo.hit_end <= p.end)""",
+                        locals())
     grouped2 = merged_mast.groupby(['chr', 'start', 'end'])
     # TODO: aggregate matrix_id
     grouped_mast = grouped2.agg({'matrix_score': max}).reset_index()
 
-    table2 = sqldf("""   SELECT t.*,
+    tbl = sqldf("""   SELECT t.*,
                                 gm.matrix_score,
                                 gs.MACS_Score, gs.fold_enrichment
                            FROM tbl AS t
@@ -93,19 +110,19 @@ def generate_master_table(options):
                       LEFT JOIN grouped_samples AS gs
                                 ON (gs.chr = t.chr
                                     AND gs.start = t.start
-                                    AND gs.end = t.end)""", locals())
-    table2.rename(columns={'matrix_score': 'Max Matrix Score',
-                           'MACS_Score': 'Max MACS Score',
-                           'fold_enrichment': 'Max Fold Enrichment'},
-                  inplace=True)
+                                    AND gs.end = t.end)""",
+                locals())
+    tbl.rename(columns={'matrix_score': 'Max Matrix Score',
+                        'MACS_Score': 'Max MACS Score',
+                        'fold_enrichment': 'Max Fold Enrichment'},
+               inplace=True)
 
-    table2['Repeat Count'] = [seq_record.seq.count('N') for seq_record
-                              in SeqIO.parse(options.fasta_file, 'fasta')]
-    table2['Length'] = [len(seq_record) for seq_record
-                        in SeqIO.parse(options.fasta_file, 'fasta')]
-    table2['Repeat Proportion'] = 1.0 * table2['Repeat Count'] / table2['Length']
-
-    table2.to_csv(options.out_file, sep="\t", index=False)
+    tbl['Repeat Count'] = [seq_record.seq.count('N') for seq_record
+                           in SeqIO.parse(options.fasta_file, 'fasta')]
+    tbl['Length'] = [len(seq_record) for seq_record
+                     in SeqIO.parse(options.fasta_file, 'fasta')]
+    tbl['Repeat Proportion'] = 1.0 * tbl['Repeat Count'] / tbl['Length']
+    return tbl
 
 
 if __name__ == "__main__":
@@ -125,4 +142,5 @@ if __name__ == "__main__":
     parser.add_option('--sample_col', choices=['macs', 'fe'],
                       help='value for sample column')
     (options, args) = parser.parse_args()
-    generate_master_table(options)
+    master_table = generate_master_table(options)
+    master_table.to_csv(options.out_file, sep='\t', index=False)
