5a211acc193e2d44975d2e16c9b162ff19441113
Victor Lin <vlin96@gmail.com>
2018-09-02 14:41:01 -0400

    Add bed module tests

---
+++ test_bed.py
@@ -0,0 +1,10 @@
+from scripts.bed import Interval
+
+
+def test_bed_module():
+    assert Interval('chr2L', 5, 11).overlaps(Interval('chr2L', 10, 20))
+    assert not Interval('chr2L', 5, 10).overlaps(Interval('chr2L', 10, 20))
+    assert Interval('chr2L', 19, 30).overlaps(Interval('chr2L', 10, 20))
+    assert not Interval('chr2L', 20, 30).overlaps(Interval('chr2L', 10, 20))
+    assert Interval('chr2L', 11, 19).overlaps(Interval('chr2L', 10, 20))
+    assert Interval('chr2L', 5, 30).overlaps(Interval('chr2L', 10, 20))
