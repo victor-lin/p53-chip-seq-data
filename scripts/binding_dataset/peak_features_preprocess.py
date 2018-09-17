5e35e7e925a3b9b86db44aaf0e496b340ffb743c
Victor Lin <vlin@ufl.edu>
2018-09-16 23:28:50 -0400

    Min-max scale all features
    ref issue #15

--- scripts/binding_dataset/peak_features_preprocess.py
+++ scripts/binding_dataset/peak_features_preprocess.py
@@ -2,6 +2,8 @@ import argparse
 import pandas as pd
 import numpy as np
 
+from sklearn.preprocessing import MinMaxScaler
+
 np.random.seed(0)
 
 
@@ -16,12 +18,20 @@ def output_features(data_file, test_size, train_out, test_out):
     ids = list(set(df['id'].values))
     num_test = int(len(ids) * test_size)
     ids_test = np.random.choice(ids, size=num_test, replace=False)
+
+    cols_X = list(df.columns)
+    for col in ['binding', 'id', 'sample_count_distinct']:
+        cols_X.remove(col)
+    scaler = MinMaxScaler()
+
     df_test = df[df['id'].isin(ids_test)].copy()
     df_test.drop(['id', 'sample_count_distinct'], axis=1, inplace=True)
+    df_test[cols_X] = scaler.fit_transform(df_test[cols_X])
     df_test.to_csv(test_out, sep='\t', index=False)
 
     df_train = df[~df['id'].isin(ids_test)].copy()
     df_train.drop(['id', 'sample_count_distinct'], axis=1, inplace=True)
+    df_train[cols_X] = scaler.fit_transform(df_train[cols_X])
     df_train.to_csv(train_out, sep='\t', index=False)
 
 if __name__ == "__main__":
